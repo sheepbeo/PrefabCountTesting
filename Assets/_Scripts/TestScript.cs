fede1896c95393ca7f8f19a7cf8df27a145ac008
sheepbeo <lequoccuong91@yahoo.com>
2015-07-21 14:22:00 +0300

    initial

---
+++ Assets/_Scripts/TestScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class TestScript : MonoBehaviour {
+
+	public int Count = 1;
+	
+	public GameObject NormalPrefab;
+	
+	void Start() 
+	{
+		DateTime begin = DateTime.UtcNow;
+		
+		TestNormal ();
+		//TestFactory ();
+		
+		print ("time elapsed : " + (DateTime.UtcNow - begin).TotalSeconds);
+	}
+	
+	private void TestNormal() {
+		for (int i=0; i<Count; i++) {
+			NormalCreateInstance ();
+		}
+	}
+	
+	private void TestFactory() {
+		for (int i=0; i<Count; i++) {
+			FactoryCreateInstance ();
+		}
+	}
+	
+	private GameObject NormalCreateInstance() {
+		GameObject instance = Instantiate (NormalPrefab);
+		return instance;
+	}
+	
+	private GameObject FactoryCreateInstance() 
+	{
+		GameObject instance = new GameObject ("Instance");
+		PrefabScript prefab = instance.AddComponent<PrefabScript> ();
+
+		string path = "_Textures/t";
+		List<Sprite> list = new List<Sprite> ();
+
+		for (int i=1; i<=40; i++) {
+			Sprite s = Resources.Load<Sprite>(path + (i) + ".png");
+			list.Add(s);
+		}
+
+		prefab.Sprites = list.ToArray ();
+			
+		return instance;
+	}
+}
